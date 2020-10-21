899d48b48a93d955e4d58522688cac16ac002db5
Roy Margalit <roy4499@gmail.com>
2020-10-21 12:29:55 +0300

    second commit selecting photos and putting in canvas

--- js/controller.js
+++ js/controller.js
@@ -1,2 +1,32 @@
 'use strict'
 console.log('controller');
+
+
+function onInit() {
+    init()
+    renderGallery()
+}
+
+function renderGallery() {
+    var strHtml = ''
+    gImgs.forEach(image => {
+        strHtml += `<img src= "${image.url}" onclick="getImg(${image.id})" class= "img${image.id}"></img>`
+        // console.log(image.id)
+    });
+    var image = document.querySelector('.gallery-container')
+    image.innerHTML = strHtml
+}
+
+function getImg(imgId){
+    console.log(imgId)
+    gMeme.selectedImgId=imgId
+    console.log(gMeme)
+    drawImg()
+}
+function drawImg() {
+    var img = new Image()
+    img.src = `./imgs/${gMeme.selectedImgId}.jpg`;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+    }
+}
--- js/services.js
+++ js/services.js
@@ -1,6 +1,35 @@
 'use strict'
 console.log('services')
-var gImgs=[
-    {id:1,url: `imgs/"${1.jpg}"`}
+// var gCanvas;
+// var gCtx;
+var gCanvas = document.querySelector('#meme-canvas');
+var gCtx = gCanvas.getContext('2d');
+
+var gImgs = [
+    { id: 1, url: './imgs/1.jpg' },
+    { id: 2, url: './imgs/2.jpg' },
+    { id: 3, url: './imgs/3.jpg' },
+    { id: 4, url: './imgs/4.jpg' },
+    { id: 5, url: './imgs/5.jpg' },
+    { id: 6, url: './imgs/6.jpg' },
 ]
-console.log(gImgs)
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines:[
+        {
+            txt:'HUMMUS!',
+            size:20,
+            align:'left',
+            color:'red'
+        }
+    ]
+}
+
+
+function init() {
+    gCanvas = document.querySelector('#meme-canvas');
+    gCtx = gCanvas.getContext('2d');
+    
+}
